4c815734d8283ba8ff11411e619b85fb08428322
Gunther <hkngae@connect.ust.hk>
2023-09-15 21:25:33 +0800

    Add 2 lines to report heading

--- src/main/java/Lab1/mainApp1.java
+++ src/main/java/Lab1/mainApp1.java
@@ -9,6 +9,8 @@ public class mainApp1 {
     public static void main(String[] args) {
         int b = 2;
         int n = 11;
+        System.out.println("Welcome to Scientific Calculator!");
+        System.out.println("Program started ...");
         System.out.println(b + " to power " + n + "=" + myLibrary.Power(b, n));
         System.out.println(n + "! = " + myLibrary.factorial(n));
         System.out.println("Program ..Ended ...");
